461735995890e1592d0c3671dbda635f3f52ae04
Applejack16 <44654338+Applejack16@users.noreply.github.com>
2019-07-31 00:09:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,16 +7,16 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content.startsWith(`${prefix}ping`)) {
+    if (message.content.startsWith(`CB{prefix}ping`)) {
         message.channel.send('Pong.');
     }
-    else if (message.content.startsWith(`${prefix}beep`)) {
+    else if (message.content.startsWith(`CB{prefix}beep`)) {
         message.channel.send('Boop.');
     }
-    else if (message.content === `${prefix}server`) {
+    else if (message.content === `CB{prefix}server`) {
         message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\nCreated at: ${message.guild.createdAt}\nRegion: ${message.guild.region}`);
     }
-    else if (message.content === `${prefix}user-info`) {
+    else if (message.content === `CB{prefix}user-info`) {
         message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
     };
 });
