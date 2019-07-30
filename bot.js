b8af5595d3fc270bb2b5c4ce7a060de2c5c894c2
Applejack16 <44654338+Applejack16@users.noreply.github.com>
2019-07-31 01:23:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,14 @@
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('Ready!');
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-    if (message.content.startsWith(`${prefix}ping`)) {
-        message.channel.send('Pong.');
-    }
-    else if (message.content.startsWith(`${prefix}beep`)) {
-        message.channel.send('Boop.');
-    }
-    else if (message.content === `${prefix}server`) {
-        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\nCreated at: ${message.guild.createdAt}\nRegion: ${message.guild.region}`);
-    }
-    else if (message.content === `${prefix}user-info`) {
-        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-    };
+client.on('message', msg => {
+  if (msg.content === 'CBping') {
+    msg.reply('Pong!');
+  }
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login('Paste the Bot Token here');
